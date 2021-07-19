3be4655887218f6688696eebfd4446d7e82f8cd7
Shawn Roller <sroller@techstyle.com>
2021-07-19 16:59:35 -0400

    adds caching mechanism

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useEffect } from 'react';
+import React from 'react';
 import { StyleSheet, Text, View, Pressable } from 'react-native';
 import Video from "react-native-video";
+import convertToProxyURL from "react-native-video-cache";
 
-const URL = "https://rawgit.com/mediaelement/mediaelement-files/master/big_buck_bunny.mp4";
+const URL = "https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-720p.mp4";
 
 export default function App() {
 
@@ -17,18 +18,12 @@ export default function App() {
 
   const onSave = async () => {
     if (player) {
+      // Fails to save...
       const response = await player.save();
       const uri = response.uri;
       console.log(`here's the uri: ${uri}`);
     }
   }
-
-  useEffect(() => {
-    if (player) {
-      // attempt to save the video for caching
-      player.save();
-    }
-  }, [player]);
 
   return (
     <View style={styles.container}>
@@ -36,10 +31,9 @@ export default function App() {
       <Pressable onPress={onPressPlay}><Text>video button</Text></Pressable>
       <Pressable onPress={onSave}><Text>save</Text></Pressable>
       <Video
-        source={{ uri: URL }}
+        source={{ uri: convertToProxyURL(URL) }}
         ref={(ref) => player = ref}
-        // style={{ position: "absolute", width: 200, height: 200, top: 0 }}
-        style={{ position: "absolute", top: 0, bottom: 0, backgroundColor: "blue", start: -110, end: -110 }}
+        style={{ position: "absolute", width: 200, height: 200, top: 0 }}
         onBuffer={() => console.log("buffering....")}
         onError={() => console.log("error occurred")}
         repeat
